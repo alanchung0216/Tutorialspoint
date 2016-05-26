45eef2464ef10cb36581de02cc51499dfa33011d
alanchung <alanchung@alans-imac.home>
2016-05-25 23:37:39 -0400

    checkin

--- src/com/tutorialspoint/Directories/DirectorySize.java
+++ src/com/tutorialspoint/Directories/DirectorySize.java
@@ -1,14 +1,14 @@
 package com.tutorialspoint.Directories;
 
 import java.io.File;
-import org.apache.commons.io.FileUtils;
+//import org.apache.commons.io.FileUtils;
 
 public class DirectorySize {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		File fh = new File("C:/dj");
-		long size = FileUtils.sizeOfDirectory(fh);
-		System.out.println("size is "+size);
+		File fh = new File("~/Documents/workspace");
+//		long size = FileUtils.sizeOfDirectory(fh);
+//		System.out.println("size is "+size);
 	}
 }
